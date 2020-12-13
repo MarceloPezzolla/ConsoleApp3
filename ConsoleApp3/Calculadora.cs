223cb3c6109d398e0a2b9926390ab7d20fada43b
MarceloPezzolla <marcelopezzolla@gmail.com>
2020-12-13 20:28:34 -0300

    Calculadora

--- ConsoleApp3/Calculadora.cs
+++ ConsoleApp3/Calculadora.cs
@@ -1,20 +1,88 @@
-﻿namespace Projeto
+﻿using System;
+using System.Globalization;
+
+namespace Projeto
 {
     public class Calculadora
     {
-        // Soma(double n1, double n2)
-        public double Soma(double n1, double n2)
+        public void Inicio()
         {
-            return n1 + n2;
-        }
-        public double Soma(params double[] numeros)
-        {
+
+            double n1 = 0.0;
+            double n2 = 0.0;
             double resultado = 0.0;
-            foreach (double numero in numeros)
+
+            Calculadora calc;
+            calc = new Calculadora();
+
+            Console.WriteLine("Digite um valor:");
+            n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Qual operação você deseja fazer?");
+            string operacao = Console.ReadLine();
+
+            switch (operacao)
             {
-                resultado += numero;
+                case "+":
+                    Soma(ref n1, out n2, out resultado);
+                    break;
+                case "-":
+                    Subtracao(ref n1, out n2, out resultado);
+                    break;
+                case "x":
+                    Multiplicacao(ref n1, out n2, out resultado);
+                    break;
+                case "/":
+                    Divisao(ref n1, out n2, out resultado);
+                    break;
+                default:
+                    Console.WriteLine("Digite uma opção válida");
+                    Console.ReadLine();
+                    break;
             }
-            return resultado;
+        }
+    public void Soma(ref double n1, out double n2, out double resultado)
+        {
+            Console.WriteLine("Digite o Segundo valor:");
+            n2 = double.Parse(Console.ReadLine());
+
+            resultado = (n1 + n2);
+
+            Console.WriteLine("Resultado = " + resultado);
+        }
+        public void Subtracao(ref double n1,  out double n2, out double resultado)
+        {
+            Console.WriteLine("Digite o Segundo valor:");
+            n2 = double.Parse(Console.ReadLine());
+
+            resultado = (n1 - n2);
+
+            Console.WriteLine("Resultado = " + resultado);
+        }
+        public void Multiplicacao(ref double n1, out double n2, out double resultado)
+        {
+            Console.WriteLine("Digite o Segundo valor:");
+            n2 = double.Parse(Console.ReadLine());
+
+            resultado = (n1 * n2);
+
+            Console.WriteLine("Resultado = "+ resultado);
+        }
+        public void Divisao(ref double n1, out double n2, out double resultado)
+        {
+            Console.WriteLine("Digite o Segundo valor:");
+            n2 = double.Parse(Console.ReadLine());
+            resultado = 0.0;
+            if (n2 == 0)
+            {
+                Console.WriteLine("Operação Inválida! Impossivel dividir por 0!");
+            }
+            else
+            {
+                resultado = (n1 / n2);
+
+                Console.WriteLine("Resultado = " + resultado);
+            }
+            
         }
     }
 }
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,22 +7,8 @@ namespace Projeto
     { 
         public static void Main(String[] args)
         {
-            Calculadora calc;
-            calc = new Calculadora();
-
-            double[] numeros = { 1.2, 1.3, 1.4, 2.4, 4.4 };
-
-            double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double resultado = calc.Soma(n1, n2);
-            Console.WriteLine("resultado 1 = " + resultado);
-
-
-            double resultado2 = calc.Soma(numeros);
-            Console.WriteLine("resultado 2 =" + resultado2);
-
-
+            Calculadora calc = new Calculadora();
+            calc.Inicio();
             Console.Read();
         }
     }
